eec3164af69e9a6941e48b0916816106aeaca390
Francis Pagaragan <fjpagaragan@gmail.com>
2024-02-01 20:48:24 +0800

    Initial commit

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,13 @@
 import "@/styles/globals.css";
+import { Box, ChakraProvider } from "@chakra-ui/react";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <ChakraProvider>
+      <Box maxW="container.xl" mx={'auto'} p={'4'}>
+        <Component {...pageProps} />
+      </Box>
+    </ChakraProvider>
+    );
 }
