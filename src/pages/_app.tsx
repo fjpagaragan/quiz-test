de455c1584d68ac5919c71daee7689e1f4653662
Francis Pagaragan <fjpagaragan@gmail.com>
2024-02-02 16:25:55 +0800

    (feat): created questionnaire component, custom radio button for choices

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,12 +2,15 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { Box, CSSReset, ChakraProvider, Container } from "@chakra-ui/react";
 import NavBar from "@/common/components/NavBar";
+import { useRouter } from "next/router";
 
 export default function App({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+  const showNavBar = router.pathname !== '/quiz';
   return (
     <ChakraProvider>
       <CSSReset />
-      <NavBar />
+      {showNavBar && <NavBar />}
       <Box maxW="container.2xl" mx="auto" my={'auto'} p={4}>
         <Component {...pageProps} />
       </Box>
---
+++ src/pages/quiz/index.tsx
@@ -0,0 +1,8 @@
+import { NextPage } from "next";
+import Quiestionnaire from "@/modules/quiz/components/Questionnaire";
+
+const QuizPage: NextPage = () => {
+    return <Quiestionnaire />
+}
+
+export default QuizPage;
