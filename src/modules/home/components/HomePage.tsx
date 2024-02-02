b872eeb8b6e61b8b2c810fe6169babd2368a964d
Francis <fjpagaragan@gmail.com>
2024-02-02 13:20:15 +0800

    (feat): add quiz overview component (#2)

--- src/modules/home/components/HomePage.tsx
+++ src/modules/home/components/HomePage.tsx
@@ -1,14 +1,22 @@
 import { FC } from "react";
+import { useRouter } from "next/router";
 import { AbsoluteCenter, Box, Button, Center, Flex, Heading, Stack, Text } from "@chakra-ui/react";
 
+
 const HomePage: FC = () => {
+    const router = useRouter();
+    
+    const directToQuizHandler = () => {
+        router.push('/overview');
+    }
+
     return (
         <Flex height={"90vh"} alignContent={"center"} justifyContent={"center"}>
             <Center>
                 <Stack as={Box} textAlign={'center'}>
                     <Heading as="h2" size="xl" mt={6} mb={2}>English Mastery Begins Here</Heading>
                     <Text color={'gray.500'} noOfLines={2}>Immerse yourself in our dynamic quizzes and conquer the challenges of QuizTest English exams.</Text>
-                    <Button color={'white'} bg={'#5F2D75'} _hover={{ border: "2px", borderColor: "#5F2D75", background: "white", textColor: "#5F2D75" }}>Begin Your English Journey</Button>
+                    <Button onClick={directToQuizHandler} color={'white'} bg={'#5F2D75'} _hover={{ border: "2px", borderColor: "#5F2D75", background: "white", textColor: "#5F2D75" }}>Begin Your English Journey</Button>
                 </Stack>
             </Center>
         </Flex>
---
+++ src/pages/overview/index.tsx
@@ -0,0 +1,8 @@
+import { NextPage } from "next";
+import Overview from "@/modules/quiz/components/Overview";
+
+const QuizPage: NextPage = () => {
+    return <Overview />
+}
+
+export default QuizPage;
