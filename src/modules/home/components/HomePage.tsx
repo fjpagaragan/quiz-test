3fc983295abedee291d2efb47fd6a18a6fc9e96c
Francis Pagaragan <fjpagaragan@gmail.com>
2024-02-03 22:52:03 +0800

    (feat): change layout homepage

--- src/modules/home/components/HomePage.tsx
+++ src/modules/home/components/HomePage.tsx
@@ -1,6 +1,7 @@
 import { FC } from "react";
 import { useRouter } from "next/router";
-import { AbsoluteCenter, Box, Button, Center, Flex, Heading, Stack, Text } from "@chakra-ui/react";
+import { AbsoluteCenter, Box, Button, Center, Flex, Heading, SimpleGrid, Stack, Text } from "@chakra-ui/react";
+import HomePageImage from "common/images/HomePageImage";
 
 
 const HomePage: FC = () => {
@@ -11,16 +12,19 @@ const HomePage: FC = () => {
     }
 
     return (
-        <Flex height={"90vh"} alignContent={"center"} justifyContent={"center"}>
-            <Center>
-                <Stack as={Box} textAlign={'center'}>
+        <Flex alignContent={'center'} justifyContent={'center'} mt={'60px'} height={{ 'sm': 'auto', 'lg': '90vh'}}>
+            <SimpleGrid columns={2} spacing={10}>
+                <Stack as={Box} textAlign={'start'}>
                     <Heading as="h2" mt={6} mb={2} fontStyle={'Source Sans Pro'} fontSize={'64px'} fontWeight={700}>English Mastery Begins Here</Heading>
                     <Text color={'gray.500'} fontStyle={'Inter'} fontSize={'28px'} fontWeight={400}>Immerse yourself in our dynamic quizzes and <br />conquer the challenges of University English exams.</Text>
-                    <Box>
+                    <Box mt={10}>
                         <Button onClick={directToQuizHandler} color={'white'} bg={'#5F2D75'} _hover={{ border: "2px", borderColor: "#5F2D75", background: "white", textColor: "#5F2D75" }}>Begin Your English Journey</Button>
                     </Box>
                 </Stack>
-            </Center>
+                <Box>
+                    <HomePageImage />
+                </Box>
+            </SimpleGrid>
         </Flex>
     )
 };
