3e5d88c72531ad91d53117b1359aade7fe518cc4
Terrence <duhaylungso@consoleconnect.com>
2024-10-10 10:31:05 +0800

    v1.0.1

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,15 +5,16 @@ import NavBar from "@components/NavBar";
 import { useRouter } from "next/router";
 
 export default function App({ Component, pageProps }: AppProps) {
-  const router = useRouter();
-  const showNavBar = router.pathname !== '/quiz';
-  return (
-    <ChakraProvider>
-      <CSSReset />
-      {showNavBar && <NavBar />}
-      <Box maxW="container.2xl" mx="auto" my={'auto'} p={4}>
-        <Component {...pageProps} />
-      </Box>
-    </ChakraProvider>
-    );
+	const router = useRouter();
+	const showNavBar = router.pathname !== "/quiz";
+	return (
+		<ChakraProvider>
+			<CSSReset />
+			{showNavBar && <NavBar />}
+			<Box maxW="container.2xl" mx="auto" my={"auto"} p={4}>
+				<Component {...pageProps} />
+			</Box>
+			<p>test</p>
+		</ChakraProvider>
+	);
 }
